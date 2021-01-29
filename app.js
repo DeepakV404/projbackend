158a562e5c0dcd1848c0acf3ab33e42f5d177bf4
Deepak <17deepakrv5@gmail.com>
2021-01-29 22:12:12 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app= express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {
+    console.log("Connected to DB")
+});
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`App is running at port ${port}`);
+})
